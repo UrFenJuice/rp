1ab155a6cf377474e294f1e56fdc773e38f12138
Yuri Shuin <wuin69@gmail.com>
2017-03-09 16:49:45 +0300

    merge from mac

--- gulp/tasks/sprite.js
+++ gulp/tasks/sprite.js
@@ -3,13 +3,16 @@
 module.exports = function() {
 	$.gulp.task('sprite', function () {
 	  var spriteData = $.gulp.src('./source/sprite/**/*.png').pipe($.gp.spritesmith({
+	    retinaSrcFilter: ['./source/sprite/**/*2x.png'],
 	    imgName: 'sprite.png',
+	    retinaImgName: 'sprite2x.png',
 	    cssName: 'sprite.scss',
 	    cssFormat: 'css',
 	    imgPath: '/assets/img/sprite.png',
 	    cssTemplate: './source/sprite_template/scss.template.handlebars',
 	    padding : 0,
-	    algorithm: 'diagonal'
+	    algorithm: 'top-down'
+	    
 	  }));
 	  return spriteData.img.pipe($.gulp.dest($.config.root + '/assets/img')),
 	  		 spriteData.css.pipe($.gulp.dest('./source/style/common'));
