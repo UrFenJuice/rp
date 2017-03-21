565edfb983231eb1f4758b26ffe6b2aaeeb327d7
Yuri Shuin <wuin69@gmail.com>
2017-03-21 16:54:30 +0300

    add bs select

--- gulp/paths/css.foundation.js
+++ gulp/paths/css.foundation.js
@@ -2,5 +2,6 @@
 
 module.exports = [
   './node_modules/normalize.css/normalize.css',
-  './node_modules/jquery.scrollbar/jquery.scrollbar.css'
+  './node_modules/jquery.scrollbar/jquery.scrollbar.css',
+  './node_modules/bootstrap-select/dist/css/bootstrap-select.min.css'
 ];
--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -3,6 +3,7 @@
 module.exports = [
   //'./bower_components/jquery/dist/jquery.min.js',
   './node_modules/jquery.scrollbar/jquery.scrollbar.min.js',
-  './node_modules/svg4everybody/dist/svg4everybody.min.js'
+  './node_modules/svg4everybody/dist/svg4everybody.min.js',
+  './node_modules/bootstrap-select/dist/js/bootstrap-select.min.js'
   //'./node_modules/flexibility/flexibility.js'
 ];
