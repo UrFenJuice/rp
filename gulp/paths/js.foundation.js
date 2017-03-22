e9edf8fc4aa3efcc6fbbe7bc98760a4ad1bc95ed
Yuri Shuin <wuin69@gmail.com>
2017-03-22 11:39:32 +0300

    add select + bootstrap

--- gulp/paths/js.foundation.js
+++ gulp/paths/js.foundation.js
@@ -4,6 +4,7 @@ module.exports = [
   //'./bower_components/jquery/dist/jquery.min.js',
   './node_modules/jquery.scrollbar/jquery.scrollbar.min.js',
   './node_modules/svg4everybody/dist/svg4everybody.min.js',
+  './node_modules/bootstrap/dist/js/bootstrap.min.js',
   './node_modules/bootstrap-select/dist/js/bootstrap-select.min.js'
   //'./node_modules/flexibility/flexibility.js'
 ];
