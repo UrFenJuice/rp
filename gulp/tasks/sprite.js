ae670e6d3aa08bad388c83dab0b0c1a17833a695
Yuri Shuin <wuin69@gmail.com>
2017-04-04 16:15:35 +0300

    add payment history - test.html

--- gulp/tasks/sprite.js
+++ gulp/tasks/sprite.js
@@ -3,13 +3,13 @@
 module.exports = function() {
 	$.gulp.task('sprite', function () {
 	  var spriteData = $.gulp.src('./source/sprite/**/*.png').pipe($.gp.spritesmith({
-	    retinaSrcFilter: ['./source/sprite/**/*2x.png'],
+	    //retinaSrcFilter: ['./source/sprite/**/*2x.png'],
 	    imgName: 'sprite.png',
-	    retinaImgName: 'sprite2x.png',
+	    //retinaImgName: 'sprite2x.png',
 	    cssName: 'sprite.scss',
 	    cssFormat: 'css',
 	    imgPath: '/assets/img/sprite.png',
-	    cssTemplate: './source/sprite_template/scss.template.handlebars',
+	    //cssTemplate: './source/sprite_template/scss.template.handlebars',
 	    padding : 0,
 	    algorithm: 'top-down'
 	    
